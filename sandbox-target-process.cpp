cd6fb8ec9ae1d05d8ce48d980b241aaa12cb85d0
zsoltk130 <Zsolt@DESKTOP-VLPBAOH>
2025-08-27 18:31:29 +0100

    Added functionality to print a list of variables with their memory addresses and values, added functionality to refresh variable list

--- sandbox-target-process.cpp
+++ sandbox-target-process.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+
+struct TestVar {
+    std::string type;
+    void* address;
+    std::string valueStr; // Printable form
+};
+
+// Global test variables
+int gInt;
+float gFloat;
+double gDouble;
+char gString[6]; // Up to 5 chars + null terminator
 
 // Helper functions
 int randomInt() { return rand() % 100000; }
@@ -13,15 +28,42 @@ std::string randomString() {
     return s;
 }
 
+void generateValues() {
+    gInt = randomInt();
+    gFloat = randomFloat();
+    gDouble = randomDouble();
+    std::string s = randomString();
+    strncpy_s(gString, s.c_str(), sizeof(gString));
+    gString[sizeof(gString) - 1] = '\0';
+}
+
+// Get variable details
+std::vector<TestVar> collectInfo() {
+    std::vector<TestVar> vars;
+    vars.push_back({ "Integer", &gInt, std::to_string(gInt) });
+    vars.push_back({ "Float", &gFloat, std::to_string(gFloat) });
+    vars.push_back({ "Double", &gDouble, std::to_string(gDouble) });
+    vars.push_back({ "String", gString, std::string(gString) });
+    return vars;
+}
+
+void printValues() {
+    auto vars = collectInfo();
+    for (auto& v : vars) {
+        std::cout << std::left << std::setw(8) << v.type
+            << " | Address: 0x" << std::hex << (uintptr_t)v.address
+            << std::dec << " | Value: " << v.valueStr << "\n";
+    }
+    std::cout << "--------------------------------------------\n";
+}
+
 int main()
 {
     srand((unsigned)time(nullptr));
-    
-    std::cout << randomInt() << std::endl;
-    std::cout << randomFloat() << std::endl;
-    std::cout << randomDouble() << std::endl;
-    std::cout << randomString() << std::endl;
 
+    generateValues();
+    printValues();
+    
     char cmd;
     while (true) {
         std::cout << "[r]efresh  [g]enerate  [q]uit > ";
@@ -29,10 +71,11 @@ int main()
 
         if (cmd == 'q') break;
         else if (cmd == 'g') {
-
+            generateValues();
+            printValues();
         }
         else if (cmd == 'r') {
-
+            printValues();
         }
         else { std::cout << "Unrecognized command.\n"; }
     }
